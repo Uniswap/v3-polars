0efe9317f37ffd78933def029be427a2482683af
Austin Adams <36116882+aadams@users.noreply.github.com>
2024-04-05 19:10:17 -0400

    feat: abstracted connector away (and ran linter) (#5)
    * feat: abstracted connector away (and ran linter)
    
    * removed references to gcp and cleaned up code
    
    * feat: added more fleshed out natspec

--- v3/helpers/data_update.py
+++ v3/helpers/data_update.py
@@ -1,13 +1,16 @@
 import polars as pl
 import os
 from datetime import date, timedelta, datetime, timezone
+from .connectors import *
 
 gcp_locked = True
 try:
     from google.cloud import bigquery
+
     gcp_locked = False
 except ImportError:
     print("Unable to import GCP")
+
 
 # data updating
 def checkPath(data_type, data_path):
@@ -19,16 +22,18 @@ def checkPath(data_type, data_path):
     if not os.path.exists(path):
         os.mkdir(path)
 
+
 def isDS_Store(f):
     """
-    All my homies hate the .DS_Store, so we check 
+    All my homies hate the .DS_Store, so we check
     if the file is .DS_Store before iterating over files
     """
     return f != ".DS_Store"
 
+
 def getHeader(table, data_path):
     """
-    Returns an increasing number that will ensure that there 
+    Returns an increasing number that will ensure that there
     are no collisions. Pulls that number from the files and
     then adds 1 to it.
     """
@@ -48,76 +53,52 @@ def getHeader(table, data_path):
     return max(max_index) + 1
 
 
-def writeDataset(df, data_type, data_path, max_block_of_segment, min_block_of_segment):
+def writeDataset(df, table, data_path, max_block_of_segment, min_block_of_segment):
     """
     Writes the given file with the given heuristics to the disk
     """
-    idx = getHeader(data_type, data_path)
+    idx = getHeader(table, data_path)
     df.write_parquet(
-        f"{data_path}/{data_type}/{idx}_{min_block_of_segment}_{max_block_of_segment}_{data_type}.parquet"
+        f"{data_path}/{table}/{idx}_{min_block_of_segment}_{max_block_of_segment}_{table}.parquet"
     )
 
 
-def readGBQ(gbq_table, max_block_of_segment, min_block_of_segment, client, chain):
+def readRemote(table, connector, max_block_of_segment, min_block_of_segment, chain):
     """
-    Pull from internal GBQ data lake
+    Read the raw dataframe from remote
     """
-    q = f"""select * 
-        FROM `{gbq_table}` 
-        WHERE chain_name = '{chain}'
-        AND block_number <= {max_block_of_segment}
-        AND block_number >= {min_block_of_segment}
-        """
 
-    query_job = client.query(q)  # API request
-    rows = query_job.result()  # Waits for query to finish
-
-    df = pl.from_arrow(rows.to_arrow())
+    q = connector.get_template(
+        "read", table, max_block_of_segment, min_block_of_segment, chain
+    )
+    df = connector.execute(q)
 
     return df
 
 
-def checkMinMaxBlock(gbq_table, client, chain):
+def checkGlobalMinMaxBlock(table, connector, chain):
     """
     Find the max and min row in the database to pull over
     """
-    # read the first entry
-    q = f"""select min(block_number) as min_block,
-                   max(block_number) as max_block,
-                   FROM `{gbq_table}` 
-                   where chain_name = '{chain}'
-         """
-    query_job = client.query(q)  # API request
-    rows = query_job.result()  # Waits for query to finish
 
-    df = pl.from_arrow(rows.to_arrow())
+    # read the first entry
+    q = connector.get_template("minMax", table, chain)
+    df = connector.execute(q)
+
     return df["max_block"].item(), df["min_block"].item()
 
 
-def findSegment(gbq_table, min_block, client, chain, tgt_max_rows):
+def findSegment(table, connector, min_block, chain, tgt_max_rows):
     """
     We want to find the smallest block such that we are pulling
     around the tgt_max_rows number of rows from GBQ
     """
-    q = f"""select max(block_number)
-            from (
-                select * 
-                  from (
-                    select block_number
-                    FROM `{gbq_table}` 
-                    where chain_name = '{chain}'
-                    and block_number >= {min_block}
-                    order by block_timestamp asc
-                  ) limit {tgt_max_rows}
-            )
-         """
 
-    query_job = client.query(q)  # API request
-    rows = query_job.result()  # Waits for query to finish
-
-    df = pl.from_arrow(rows.to_arrow())
+    q = connector.get_template("findSegment", table, min_block, chain, tgt_max_rows)
+    df = connector.execute(q)
 
     return df.item()
+
 
 def readOVM(path, data_type):
     """
@@ -135,46 +116,28 @@ def readOVM(path, data_type):
 
     return mappings
 
-def update_tables_gbq(pool, tables=[]):
-    """
-    This is the big file that pulls from the GBQ servers for the given
-    pool and requested tables
-    """
-    # bigquery strings -> python
-    proj_id = "uniswap-labs"
-    db = "on_chain_events"
-    client = bigquery.Client(project = proj_id)
 
-    tableToDB = {
-        "uniswap-labs.on_chain_events.uniswap_v3_factory_pool_created_events_combined": "factory_pool_created",
-        "uniswap-labs.on_chain_events.uniswap_v3_pool_swap_events_combined": "pool_swap_events",
-        "uniswap-labs.on_chain_events.uniswap_v3_pool_mint_burn_events_combined": "pool_mint_burn_events",
-        "uniswap-labs.on_chain_events.uniswap_v3_pool_initialize_events_combined": "pool_initialize_events",
-    }
-
+def _update_tables(pool, tables=[]):
     if tables == []:
         tables = pool.tables
 
     for table in tables:
         print(f"Starting table {table}")
-        gbq_table = f"{proj_id}.{db}.{table}"
-
-        data_type = tableToDB[gbq_table]
-        checkPath(data_type, pool.data_path)
+        checkPath(table, pool.data_path)
 
         # max row in gbq and min row in gbq
-        max_block, min_block_of_segment = checkMinMaxBlock(
-            gbq_table, client, pool.chain
+        max_block, min_block_of_segment = checkGlobalMinMaxBlock(
+            table, pool.connector, pool.chain
         )
         print(f"Found {min_block_of_segment} to {max_block}")
 
         # check if we already have data
-        header = getHeader(data_type, pool.data_path)
+        header = getHeader(table, pool.data_path)
         # we already have existing data, so lets get the bn to only append new stuff
         if header != 0:
             print(f"Found data")
             found_min_block_of_segment = (
-                pl.scan_parquet(f"{pool.path}/{data_type}/*.parquet")
+                pl.scan_parquet(f"{pool.path}/{table}/*.parquet")
                 .filter(pl.col("chain_name") == pool.chain)
                 .select("block_number")
                 .max()
@@ -196,27 +159,27 @@ def update_tables_gbq(pool, tables=[]):
             # the finds the max block of the segment
             # which is the max block that returns close to the target amount of rows to pull from gbq
             max_block_of_segment = findSegment(
-                gbq_table,
+                table,
+                pool.connector,
                 min_block_of_segment,
-                client,
                 pool.chain,
                 pool.tgt_max_rows,
             )
 
             print(f"Going from {min_block_of_segment} to {max_block_of_segment}")
-            # read that segment in from gbq
-            df = readGBQ(
-                gbq_table,
+            # read that segment in from remote
+            df = readRemote(
+                table,
+                pool.connector,
                 max_block_of_segment,
                 min_block_of_segment,
-                client,
                 pool.chain,
             )
 
             # save it down
             writeDataset(
                 df,
-                data_type,
+                table,
                 pool.data_path,
                 max_block_of_segment,
                 min_block_of_segment,
@@ -248,7 +211,7 @@ def update_tables_gbq(pool, tables=[]):
                     .cast({"block_number": pl.Int64})
                 )
 
-                if data_type in [
+                if table in [
                     "pool_swap_events",
                     "pool_mint_burn_events",
                     "pool_initialize_events",
@@ -259,7 +222,7 @@ def update_tables_gbq(pool, tables=[]):
                         address=pl.col("address").map_dict(mapping, default=None)
                     )
 
-                elif data_type in ["factory_pool_created"]:
+                elif table in ["factory_pool_created"]:
                     df = df.with_columns(
                         # ovm changed contract addresses from ovm1 to ovm2
                         # we map this back for us
@@ -267,7 +230,7 @@ def update_tables_gbq(pool, tables=[]):
                     )
 
                 # we index the optimism chain by backloading all the ovm1 data as optimism at block 0
-                writeDataset(df, data_type, pool.data_path, 0, 0)
+                writeDataset(df, table, pool.data_path, 0, 0)
 
             # this moves the iteration, we pulled all of block n, so we want to start at n+1
             min_block_of_segment = max_block_of_segment + 1
@@ -283,11 +246,14 @@ def update_tables_cryo(pool, tables=[]):
     # TODO
     raise NotImplementedError("Cryo is not yet implimented")
 
+
 def update_tables(pool, update_from, tables=[]):
-    if update_from == 'gcp':
+    if update_from == "gcp":
         assert not gcp_locked, "GCP could not be imported"
-        update_tables_gbq(pool, tables)
-    elif update_from == 'cryo':
-        update_tables_cryo(pool, tables)
+        pool.connector = gbq()
+        _update_tables(pool, tables)
+
+    elif update_from == "cryo":
+        _update_tables(pool, tables)
     else:
         raise NotImplementedError("Data puller not implimented")
--- v3/state.py
+++ v3/state.py
@@ -1,14 +1,16 @@
 from .helpers import *
 import polars as pl
 from pathlib import Path
+
 PACKAGEDIR = Path(__file__).parent.absolute()
+
 
 class v3Pool:
     def __init__(
         self,
         pool,
         chain,
-        update_from = 'gcp',
+        update_from="gcp",
         low_memory=False,
         update=False,
         pull=True,
@@ -39,7 +41,7 @@ class v3Pool:
         self.chain = chain
         # remove checksums
         self.pool = pool.lower()
-        
+
         # this is the cache where we store data if needed
         self.cache = {}
         self.cache["as_of"] = 0
@@ -52,12 +54,13 @@ class v3Pool:
         # we strip the "uniswap_v3_factory"
         # and "uniswap_v3_pool" bc its unneeded
         self.tables = [
-            "uniswap_v3_factory_pool_created_events_combined",
-            "uniswap_v3_pool_swap_events_combined",
-            "uniswap_v3_pool_mint_burn_events_combined",
-            "uniswap_v3_pool_initialize_events_combined",
+            "factory_pool_created",
+            "pool_swap_events",
+            "pool_mint_burn_events",
+            "pool_initialize_events",
         ]
 
+        self.connector = None
         # data quality assurances
         self.max_supported = -1
 
@@ -77,21 +80,18 @@ class v3Pool:
                 finally:
                     self.chain = "optimism"
 
-        
         self.ts, self.fee, self.token0, self.token1 = initializePoolFromFactory(
             self.pool, self.chain, self.data_path
         )
 
         if pull:
-            self.readFromMemoryOrDisk(
-                "pool_swap_events", self.data_path, save=True
-            )
+            self.readFromMemoryOrDisk("pool_swap_events", self.data_path, save=True)
             self.readFromMemoryOrDisk(
                 "pool_mint_burn_events", self.data_path, save=True
             )
 
-            max_bn_of_swaps = self.cache['swaps'].select('block_number').max().item()
-            max_bn_of_mb = self.cache['mb'].select('block_number').max().item()
+            max_bn_of_swaps = self.cache["swaps"].select("block_number").max().item()
+            max_bn_of_mb = self.cache["mb"].select("block_number").max().item()
 
             self.max_supported = min(max_bn_of_mb, max_bn_of_swaps)
 
@@ -99,13 +99,13 @@ class v3Pool:
         """
         See pool_helpers.drop_tables
         """
-        drop_tables(self, tables) 
+        drop_tables(self, tables)
 
     def readFromMemoryOrDisk(self, data, data_path, save=False):
         """
         Function that either returns a cached version for speed of
         the dataset or calculates them on the fly. This is used by all
-        points that read data to 
+        points that read data to
 
         Notice: data is the table
         Notice: data_path is the saved path to the data
@@ -114,29 +114,7 @@ class v3Pool:
         if data == "pool_swap_events":
             if "swaps" in self.cache.keys():
                 return self.cache["swaps"]
-            
-            else:
-                df = (
-                        pl.scan_parquet(f"{data_path}/{data}/*.parquet")
-                        .filter(
-                            (pl.col("address") == self.pool)
-                            & (pl.col("chain_name") == self.chain)
-                        )
-                        .with_columns(
-                            as_of=pl.col("block_number") + pl.col("transaction_index") / 1e4
-                        )
-                        .collect()
-                        .sort("as_of")
-                    )
-                if save:
-                    self.cache["swaps"] = df
 
-                return df 
-
-        elif data == "pool_mint_burn_events":
-            if "mb" in self.cache.keys():
-                return self.cache["mb"]
-            
             else:
                 df = (
                     pl.scan_parquet(f"{data_path}/{data}/*.parquet")
@@ -144,10 +122,36 @@ class v3Pool:
                         (pl.col("address") == self.pool)
                         & (pl.col("chain_name") == self.chain)
                     )
-                    .cast({"amount": pl.Float64,
+                    .with_columns(
+                        as_of=pl.col("block_number") + pl.col("transaction_index") / 1e4
+                    )
+                    .collect()
+                    .sort("as_of")
+                )
+                if save:
+                    self.cache["swaps"] = df
+
+                return df
+
+        elif data == "pool_mint_burn_events":
+            if "mb" in self.cache.keys():
+                return self.cache["mb"]
+
+            else:
+                df = (
+                    pl.scan_parquet(f"{data_path}/{data}/*.parquet")
+                    .filter(
+                        (pl.col("address") == self.pool)
+                        & (pl.col("chain_name") == self.chain)
+                    )
+                    .cast(
+                        {
+                            "amount": pl.Float64,
                             "tick_lower": pl.Int64,
                             "tick_upper": pl.Int64,
-                            "type_of_event": pl.Float64})
+                            "type_of_event": pl.Float64,
+                        }
+                    )
                     .with_columns(
                         as_of=pl.col("block_number") + pl.col("transaction_index") / 1e4
                     )
@@ -158,7 +162,7 @@ class v3Pool:
                     self.cache["mb"] = df
 
                 return df
-        
+
     def calcSwapDF(self, as_of):
         """
         @inherit from pool_helpers.createSwapDF
@@ -166,12 +170,12 @@ class v3Pool:
         swapDFs are pre-computed datasets required for swap computation
         They are cached to optimize for multiple swaps at one as_of
 
-        Notice: as_of is the block + transaction index / 1e4. 
+        Notice: as_of is the block + transaction index / 1e4.
         Notice: Returns the value before the transaction at that index was done
         """
-        if self.cache['as_of'] == as_of:
+        if self.cache["as_of"] == as_of:
             return self.cache["swapDF"], self.cache["inRangeValues"]
-        
+
         as_of, df, inRangeValues = createSwapDF(as_of, self)
 
         self.cache["as_of"] = as_of
@@ -184,7 +188,7 @@ class v3Pool:
         """
         Helper function that returns values from columns at the desired time
 
-        Notice: as_of is the block + transaction index / 1e4. 
+        Notice: as_of is the block + transaction index / 1e4.
         Notice: Returns the value before the transaction at that index was done
         """
         pool_property = (
@@ -201,7 +205,7 @@ class v3Pool:
         """
         Returns the tick from the pool as_of the given block/transaction_index.
 
-        Notice: as_of is the block + transaction index / 1e4. 
+        Notice: as_of is the block + transaction index / 1e4.
         Notice: Returns the value before the transaction at that index was done
         """
         tick = self.getPropertyFrom(as_of, "tick")
@@ -211,12 +215,12 @@ class v3Pool:
             return None
         else:
             return int(tick)
- 
+
     def getPriceAt(self, as_of, revert_on_uninitialized=False):
         """
         Returns the sqrtPriceX96 from the pool as_of the given block/transaction_index.
 
-        Notice: as_of is the block + transaction index / 1e4. 
+        Notice: as_of is the block + transaction index / 1e4.
         Notice: Returns the value before the transaction at that index was done
         """
         price = self.getPropertyFrom(as_of, "sqrtPriceX96")
@@ -226,39 +230,39 @@ class v3Pool:
             return None
         else:
             return int(price)
-        
-    def getPriceSeries(self, as_of, frequency='6h', gas = False):
+
+    def getPriceSeries(self, as_of, frequency="6h", gas=False):
         """
         @inhert from pool_helpers.getPriceSeries
-        Create a price series resampled to the desired frequency 
+        Create a price series resampled to the desired frequency
         starting at as_of
 
-        Notice: as_of is the block + transaction index / 1e4. 
+        Notice: as_of is the block + transaction index / 1e4.
         """
         px = getPriceSeries(self, as_of, frequency, gas)
 
         return px
-    
+
     def getBNAtDate(self, as_of):
         """
         @inhert from pool_helpers.dtToBN
         Returns the last block number at that datetime
 
-        Notice: as_of is the block + transaction index / 1e4. 
+        Notice: as_of is the block + transaction index / 1e4.
         """
-        
+
         return dtToBN(as_of, self)
-    
+
     def createLiq(self, as_of):
         """
         @inhert from pool_helpers.createSwapDF
         Creates a liquidity distribution as_of that time
-        
-        Notice: as_of is the block + transaction index / 1e4. 
+
+        Notice: as_of is the block + transaction index / 1e4.
         """
-        
+
         return createLiq(as_of, self, "pool_mint_burn_events", self.data_path)
-    
+
     def swapIn(self, calldata):
         """
         @inherit from swap.swapIn
@@ -266,22 +270,22 @@ class v3Pool:
 
         Calldata takes the form:
         calldata = {# the time of the swap
-                    'as_of': as_of, 
+                    'as_of': as_of,
                     # the token address going in
-                    'tokenIn': address 
+                    'tokenIn': address
                     # the amount of tokens going in
-                    'swapIn': amount 
+                    'swapIn': amount
                     # skips the swap and calculates max amount out
                     'findMax': False,
-                    # calculates fees accured to each tick 
+                    # calculates fees accured to each tick
                     'fees': True
-                    } 
-        
-        Notice: as_of is the block + transaction index / 1e4. 
+                    }
+
+        Notice: as_of is the block + transaction index / 1e4.
         """
-        
+
         return swapIn(calldata, self)
-    
+
     @property
     def swaps(self):
         """
@@ -289,11 +293,11 @@ class v3Pool:
         """
         if not self.pull:
             return self.readFromMemoryOrDisk(
-                    "pool_swap_events", self.data_path, pull=True
-                )
+                "pool_swap_events", self.data_path, pull=True
+            )
         else:
-            return self.cache['swaps']
-    
+            return self.cache["swaps"]
+
     @property
     def mb(self):
         """
@@ -301,10 +305,10 @@ class v3Pool:
         """
         if not self.pull:
             return self.readFromMemoryOrDisk(
-                    "pool_mint_burn_events", self.data_path, pull=True
-                )
+                "pool_mint_burn_events", self.data_path, pull=True
+            )
         else:
-            return self.cache['mb']
+            return self.cache["mb"]
 
     @property
     def Q96(self):
