c9537ee6c1172ec6001528699793d64580081935
Austin Adams <austino256@gmail.com>
2024-04-05 19:09:29 -0400

    feat: added more fleshed out natspec

--- v3/helpers/connectors/template.py
+++ v3/helpers/connectors/template.py
@@ -13,7 +13,7 @@ class connector_template:
     def __init__(self):
         pass
 
-    def get_remote_table(self, table):
+    def get_remote_table(self, table) -> str:
         """
         map the local name for the event to the desired global table
 
@@ -24,15 +24,21 @@ class connector_template:
         """
         pass
 
-    def minMax(self, *args):
+    def minMax(self, *args) -> str:
         """
         We want to find the bounds of the remote database
 
         This is the min and max to iterate over for updating.
         min block is overridden to the local min block later
 
-        needs to get out
+        needs to get out:
+        two columns (max_block, min_block) that return ints
+        of the max and min block in the remote sample
 
+        NOTE: we cant unfurl this right now in the query because
+        of how the generizied query executer works
+
+        # in date_update.py
         return df["max_block"].item(), df["min_block"].item()
         """
         table, chain = args
@@ -46,16 +52,22 @@ class connector_template:
 
         return q
 
-    def findSegment(self, *args):
+    def findSegment(self, *args) -> str:
         """
         We want to only pull a customizable amount of rows to ensure there are
         no issues with memory/time outs.
 
         Probably can be optimized a bunch
 
-        needs to get out
-        
-        return df.item() 
+        needs to get out:
+        one column (any column name works because item unfurls it) 
+        return the block where there exists 1m rows below it
+
+        NOTE: we cant unfurl this right now in the query because
+        of how the generizied query executer works
+
+        # in date_update.py
+        return df.item()
         """
         table, min_block, chain, tgt_max_rows = args
         table = self.get_remote_table(table)
@@ -75,11 +87,20 @@ class connector_template:
 
         return q
 
-    def readRemote(self, *args):
+    def readRemote(self, *args) -> str:
         """
         Pull from internal GBQ data lake
 
-        returns a dataframe of polars
+        returns a dataframe of polars with the desired row schema
+        the schemas can be found in data/examples/{table}
+
+        NOTE: generally these look like 
+        events with block info + tx index + gas costs
+
+        specific schemas can be found in those examples 
+
+        # in date_update.py
+        return df
         """
         table, max_block_of_segment, min_block_of_segment, chain = args
         table = self.get_remote_table(table)
@@ -93,9 +114,15 @@ class connector_template:
 
         return q
 
-    def get_template(self, query_type, *args):
+    def get_template(self, query_type, *args) -> str:
         """
         take the query type and pass the args to the right func
+
+        this is then passed back to function and then execute() is
+        called on it.
+
+        TODO:
+        check if i should just unite execute and get_template into one function
         """
         if query_type == "minMax":
             return self.minMax(*args)
@@ -106,7 +133,7 @@ class connector_template:
         else:
             raise ValueError("Missing table definition")
 
-    def execute(self, q):
+    def execute(self, q) -> pl.DataFrame:
         """
         Take the query and actually execute it
         and get the polars df return
